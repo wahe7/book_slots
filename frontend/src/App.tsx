704ef826f60ff40165afd40d342fcfab85405de2
wahe7 <waheguru002200@gmail.com>
2025-06-22 03:45:38 +0530

    set up frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-purple-100 text-2xl font-bold">
+      🎉 Tailwind CSS + TypeScript + Vite — Working!
+    </div>
+  );
+}
+
+export default App;
