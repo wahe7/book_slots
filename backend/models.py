e797a8dc97540ce6845718c968c2ea278021586e
wahe7 <waheguru002200@gmail.com>
2025-06-22 04:09:33 +0530

    Add Pydantic models for events and bookings

---
+++ backend/models.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, EmailStr
+from typing import List
+from datetime import datetime
+
+class EventCreate(BaseModel):
+    name: str
+    description: str
+    slots: List[datetime]
+    max_bookings_per_slot: int
+    
+class Event(EventCreate):
+    id: int
+    name: str
+    description: str
+    slots: List[datetime] = []
+    max_bookings_per_slot: int
+    
+    class Config:
+      orm_mode = True
+
+class CreateBooking(BaseModel):
+    name: str
+    email: EmailStr
+    slot: datetime
+    
+class Booking(BaseModel):
+    event_id: int
+    name: str
+    email: EmailStr
+    slot: datetime
+    
+    class Config:
+      orm_mode = True
+    
+    
