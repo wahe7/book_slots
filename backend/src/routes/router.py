7e94979bd5bfb3eef5a4ad446043c9b2b19b4b55
wahe7 <waheguru002200@gmail.com>
2025-06-22 23:26:09 +0530

    create router

---
+++ backend/src/routes/router.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from .events.event import router as events_router
+from .booking.bookings import router as bookings_router
+from .slot.slot import router as slots_router
+from .users.user import router as users_router
+
+router = APIRouter()
+
+# Include all route files
+router.include_router(events_router, tags=["events"])
+router.include_router(bookings_router, tags=["bookings"])
+router.include_router(slots_router, tags=["slots"])
+router.include_router(users_router, prefix="/users", tags=["users"])
