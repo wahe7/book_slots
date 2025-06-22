2915c4b0e6074a8274b721159f3ecca99093dc51
wahe7 <waheguru002200@gmail.com>
2025-06-22 23:19:35 +0530

    add new route for user booking

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreateEventPage from "./pages/CreateEventPage";
 import EventDetailsPage from "./pages/EventDetailsPage";
+import UserBookingsPage from "./pages/UserBookings";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/create-event" element={<CreateEventPage />} />
         <Route path="/events/:id" element={<EventDetailsPage />} />
+        <Route path="/user-bookings" element={<UserBookingsPage />} />
       </Routes>
     </Router>
   );
