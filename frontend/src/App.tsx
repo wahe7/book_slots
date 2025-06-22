6a01692cdef7e3c544eac4307ce7c863f275fd1e
wahe7 <waheguru002200@gmail.com>
2025-06-22 08:36:21 +0530

    add event detail page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreateEventPage from "./pages/CreateEventPage";
+import EventDetailsPage from "./pages/EventDetailsPage";
 
 export default function App() {
   return (
@@ -8,7 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create-event" element={<CreateEventPage />} />
-        {/* Add more routes as needed */}
+        <Route path="/events/:id" element={<EventDetailsPage />} />
       </Routes>
     </Router>
   );
