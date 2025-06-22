f0c21853cca651feb825a016c8f3d48f31f84600
wahe7 <waheguru002200@gmail.com>
2025-06-22 23:23:03 +0530

    slot service class

---
+++ backend/src/service/slot_service.py
@@ -0,0 +1,42 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+
+from src.db import models
+from src.schemas.slot import SlotCreate
+
+class SlotService:
+    def __init__(self, db: Session):
+        self.db = db
+    
+    def create_slot(self, slot_data: SlotCreate):
+        slot = models.Slot(time=slot_data.time, event_id=slot_data.event_id)
+        self.db.add(slot)
+        self.db.commit()
+        self.db.refresh(slot)
+        return slot
+    
+    def get_slot(self, slot_id: int):
+        slot = self.db.query(models.Slot).filter(models.Slot.id == slot_id).first()
+        if not slot:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Slot not found")
+        return slot
+    
+    def get_slots(self):
+        return self.db.query(models.Slot).all()
+    
+    def update_slot(self, slot_id: int, slot_data: SlotCreate):
+        slot = self.db.query(models.Slot).filter(models.Slot.id == slot_id).first()
+        if not slot:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Slot not found")
+        slot.time = slot_data.time
+        slot.event_id = slot_data.event_id
+        self.db.commit()
+        return slot
+    
+    def delete_slot(self, slot_id: int):
+        slot = self.db.query(models.Slot).filter(models.Slot.id == slot_id).first()
+        if not slot:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Slot not found")
+        self.db.delete(slot)
+        self.db.commit()
+        return slot
