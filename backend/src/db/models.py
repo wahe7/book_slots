b5d40e9024aec86615d5ddb6a404e7444a49e7b7
wahe7 <waheguru002200@gmail.com>
2025-06-24 01:00:25 +0530

    adding admin in model

--- backend/src/db/models.py
+++ backend/src/db/models.py
@@ -37,3 +37,11 @@ class Booking(Base):
 
     event = relationship("Event", back_populates="bookings")
     slot = relationship("Slot", back_populates="bookings")
+    
+class Admin(Base):
+    __tablename__ = "admins"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
