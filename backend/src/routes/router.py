d40e2d67f8a3ccc7ebde4049b72ae83bf43480fe
wahe7 <waheguru002200@gmail.com>
2025-06-24 01:00:40 +0530

    route for admin

--- backend/src/routes/router.py
+++ backend/src/routes/router.py
@@ -3,6 +3,7 @@ from .events.event import router as events_router
 from .booking.bookings import router as bookings_router
 from .slot.slot import router as slots_router
 from .users.user import router as users_router
+from .admin.admin import router as admin_router
 
 router = APIRouter()
 
@@ -11,3 +12,4 @@ router.include_router(events_router, tags=["events"])
 router.include_router(bookings_router, tags=["bookings"])
 router.include_router(slots_router, tags=["slots"])
 router.include_router(users_router, prefix="/users", tags=["users"])
+router.include_router(admin_router, tags=["admin"])
